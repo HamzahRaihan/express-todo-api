82ea1ca39429eb6835757934e474d4525f00f17f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-10 20:29:22 +0700

    added router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const route = express.Router();
+const UserRouter = require('./userRoutes');
+const Auth = require('../auth/auth');
 
-route.use('/', (req, res) => {
+route.get('/', (req, res) => {
   res.status(200).json({
     message: 'Todo API using Express + Mongoose',
   });
 });
 
-route.use('/todos', () => {});
+route.use('/users', UserRouter);
+route.use('/auth', Auth);
 
 module.exports = route;
