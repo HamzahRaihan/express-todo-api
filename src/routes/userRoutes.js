bb2c005b23d590dc2f4daf95d822a193828c23e3
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-10 21:54:09 +0700

    route changes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getAllUsers, register } = require('../controllers/userController');
+const { getAllUsers, register, editUser } = require('../controllers/userController');
 const route = express.Router();
 
 route.get('/', getAllUsers);
 route.post('/', register);
+route.put('/:id', editUser);
 
 module.exports = route;
