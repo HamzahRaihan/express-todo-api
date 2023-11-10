ec12215f3d31c9e158c0b0a42dfc1109c9f505a8
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-10 20:29:10 +0700

    refactor users model

--- src/models/users.js
+++ src/models/users.js
@@ -3,20 +3,21 @@ const { Schema, model } = require('mongoose');
 const UserSchema = new Schema({
   name: {
     type: String,
-    require: [true, 'Username is required'],
+    required: [true, 'Name is required'],
   },
   email: {
     type: String,
     unique: [true, 'Email is already exist'],
-    require: [true, 'Email is required'],
+    required: [true, 'Email is required'],
   },
   password: {
     type: String,
     min: [8, 'Password is too short'],
+    required: true,
   },
-  image: { type: String },
+  // image: { type: String },
 });
 
-const User = model('User', UserSchema);
+const Users = model('Users', UserSchema);
 
-module.exports = User;
+module.exports = Users;
