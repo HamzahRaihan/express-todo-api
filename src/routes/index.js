2c7251a24aff1cf3af16c5f9251fed2344421bb1
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-11 15:24:20 +0700

    Update routes to include todo functionality

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const route = express.Router();
 const UserRouter = require('./userRoutes');
-const Auth = require('../auth/auth');
+const Auth = require('../controllers/auth/auth');
+const todoRouter = require('./todoRoutes');
 
 route.get('/', (req, res) => {
   res.status(200).json({
@@ -10,6 +11,6 @@ route.get('/', (req, res) => {
 });
 
 route.use('/users', UserRouter);
-route.use('/auth', Auth);
+route.use('/todos', todoRouter);
 
 module.exports = route;
