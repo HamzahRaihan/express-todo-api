dd11250706c18dacca8c8b49423f04620776dd92
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-11 15:24:28 +0700

    Add getUserByID route and implement authentication middleware

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { getAllUsers, register, editUser } = require('../controllers/userController');
+const { getAllUsers, register, editUser, getUserByID } = require('../controllers/userController');
+const login = require('../controllers/auth/auth');
+const authChecker = require('../middleware/authChecker');
 const route = express.Router();
 
 route.get('/', getAllUsers);
+route.get('/:id', getUserByID);
 route.post('/', register);
-route.put('/:id', editUser);
+route.put('/:id', authChecker, editUser);
+route.post('/auth/login', login);
 
 module.exports = route;
